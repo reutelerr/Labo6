f36b9786c9ba51d21879cc93803b02f3eaf3218d
Unknown <loic.dessaules@gmail.com>
2018-11-28 21:16:05 +0100

    Added / modified funct. to display in a first and simple way the number
    - Display as a string representation all number except 11 - 12 - 13 - 14 - 15 - 16 
    - This is a FIRST version, so it's very awful code, but the main idea is here, I think the idea is not bad and we can continue in this way

--- Labo6/main.cpp
+++ Labo6/main.cpp
@@ -15,50 +15,149 @@
 
 using namespace std;
 
-void displayUnits(const int& num)
-{
-    
+string getStringUnits(const int& units) {
+   switch(units){
+      case 1 : 
+         return "un";
+         break;
+      case 2 : 
+         return "deux";
+         break;
+      case 3: 
+         return "trois";
+         break;
+      case 4 : 
+         return "quatre";
+         break;
+      case 5: 
+         return "cinq";
+         break;
+      case 6: 
+         return "six";
+         break;
+      case 7: 
+         return "sept";
+         break;
+      case 8: 
+         return "huit";
+         break;
+      case 9: 
+         return "neuf";
+         break;
+   }
 }
 
-void displayTens(const int& num)
-{
-    
+string getStringTens(const int& tens) {
+   switch(tens){
+      case 10 : 
+         return "dix";
+         break;
+      case 20 : 
+         return "vingt";
+         break;
+      case 30: 
+         return "trente";
+         break;
+      case 40 : 
+         return "quarante";
+         break;
+      case 50 : 
+         return "cinquante";
+         break;
+      case 60 : 
+         return "soixante";
+         break;
+      case 70 : 
+         return "septante";
+         break;
+      case 80 : 
+         return "huitante";
+         break;
+      case 90 : 
+         return "nonante";
+         break;
+   }
 }
 
-void displayHundreds(const int& num)
-{
-    if (num > 1)
-    {
-        displayUnits(num);
-        
-    }
+string getStringHundreds(const int& hundreds) {
+   string result = "cent";
+   if (hundreds > 1) {
+      result = getStringUnits(hundreds) + " " + result;
+   }
+   return result;
 }
 
 /*void displayDigit(const int& num, const int& digitIndex)*/
 
-void displayIntPart(const int& num)
-{
-    unsigned int i=0;
-    while (num)
-    {
-        displayUnits(<#const int &num#>)
-        displayTens(<#const int &num#>)
-        displayHundreds(<#const int &num#>)
-        
-        
-        num /= 1000;
-        
-    }
+string getStringEntierPart(unsigned number) {
+   string result = "";
+   unsigned units;
+   string strUnits;
+   unsigned tens;
+   string strTens;
+   unsigned hundreds;
+   string strHundreds;
+   unsigned temp;
+   
+   unsigned i = 0;
+   while (number) {
+      temp = number;
+      
+      units = temp % 10;
+      strUnits = getStringUnits(units);
+      temp /= 10;
+      
+      
+      tens = (temp % 10) * 10;
+      strTens = getStringTens(tens);
+      temp /= 10;
+      
+      hundreds = temp % 10;
+      strHundreds = getStringHundreds(hundreds);
+      temp /= 10;
+      
+      result = strUnits + " " + result;
+      result = strTens + " " + result;
+      result = strHundreds + " " + result;
+      
+      number /= 1000;
+
+   }
+   
+   return result;
 }
 
-void displayDecimalPart(const int& num)
-{
-    
+void displayDecimalPart(const int& num) {
+
 }
 
+/**
+ @brief Traduit des nombres réels en prix exprimés en vaudois
+ @param[in] montant un réel compris entre 0 et 999999.99 CHF.
+ @return une chaine de caractères indiquant en vaudois le prix
+ en francs et centimes.
+ @details Exemples:
+ 12.30  -> "douze francs et trente centimes"
+ 200.01 -> "deux cents francs et un centime"
+ 180    -> "cent huitante francs"
+ 1.80   -> "un franc et huitante centimes"
+ 0.20   -> "vingt centimes"
+ 0      -> "zéro franc"
+ */
+string montantEnVaudois(double montant) {
+   int entierPart = (int) montant;
+   double decimalPart = montant - entierPart;
+
+   return getStringEntierPart(entierPart);
+}
 
 int main() {
 
-    
-    return 0;
+   double d;
+
+   while (cin >> d) {
+      cout << montantEnVaudois(d) << endl;
+   }
+
+   return 0;
 }
